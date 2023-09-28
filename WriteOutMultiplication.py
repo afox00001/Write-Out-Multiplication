d780b928770896df94d424a91637a21b7916ec10
Ashton Fox <116043127+afox00001@users.noreply.github.com>
2023-09-28 12:56:20 -0500

    Create WriteOutMultiplication.py

---
+++ WriteOutMultiplication.py
@@ -0,0 +1,21 @@
+def WriteOutMultiplication(x,y):
+    newStr = ""
+    total = 0
+    newStr += "\n" + (str("{:,}".format(x)) + " X " + str("{:,}".format(y)) + " = ") + "\n"
+    #newStr += str("{:,}".format(x)) + " X " + str("{:,}".format(y)) + " = "
+    for i in range(y):
+        if i == 0:
+            newStr += str("{:,}".format(x))
+        else:
+            newStr += " + " + str("{:,}".format(x))
+        total += x
+    #print("=" + str("{:,}".format(total)))
+    newStr += " = " + str(total)
+    return newStr
+
+while True:
+    firstNumber = int(input("Please type the first number that you want to multiply: "))
+    secondNumber = int(input("Pleaser type what you would like to multiply your previous number by: "))
+    print()
+    print(WriteOutMultiplication(firstNumber, secondNumber))
+    print()
